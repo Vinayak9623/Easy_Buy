fe83823db1a74336cf6fb5fa35752e7b744d3c7f
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-06-07 17:36:27 +0530

    Implement Api Gateway

---
+++ api-gateway/api-gateway/src/main/java/com/vd/easybuy/apigateway/RouterConfig.java
@@ -0,0 +1,43 @@
+package com.vd.easybuy.apigateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterConfig {
+
+    private final String productServiceId;
+    private final String cartOrderServiceId;
+
+    public RouterConfig(@Value("${product.service.id}") String productServiceId,
+                        @Value("${cart-order.service.id}") String cartOrderServiceId) {
+        this.productServiceId = productServiceId;
+        this.cartOrderServiceId=cartOrderServiceId;
+    }
+
+    @Bean
+    public RouteLocator route(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route("product-route", r -> r
+                        .path("/products/**")
+                        .filters(f -> f.rewritePath(
+                                "/products/(?<remaining>.*)",
+                                "/${remaining}"
+                        ))
+                        .uri(productServiceId))
+
+
+                .route("cart-order-route",c->c
+                        .path("/cart-orders/**")
+                        .filters(f->f.rewritePath(
+                                "/cart-orders/(?<remaining>.*)",
+                                "/${remaining}"
+                        ))
+                        .uri(cartOrderServiceId))
+                .build();
+    }
+}
