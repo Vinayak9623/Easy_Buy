ed65376c4ef41ccee5d06b8aca7e315d3325ef6a
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-07-15 19:02:31 +0530

    user service createuser test cases positive case

--- Users-service/Users-service/src/test/java/com/vd/easybuy/users/service/UserServiceImplTest.java
+++ Users-service/Users-service/src/test/java/com/vd/easybuy/users/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.vd.easybuy.users.service;
 
 import com.vd.easybuy.users.dto.UserDto;
+import com.vd.easybuy.users.entity.Role;
 import com.vd.easybuy.users.entity.User;
 import com.vd.easybuy.users.exception.InvalidRequestException;
 import com.vd.easybuy.users.repository.RefreshTokenRepo;
@@ -14,8 +15,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
@@ -45,6 +45,42 @@ public class UserServiceImplTest {
         InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> userService.createUser(userDto));
         assertEquals("Email Alredy in usevinayak@gmail.com",exception.getMessage());
         Mockito.verify(userRepository,Mockito.never()).save(Mockito.any(User.class));
+
+    }
+
+    @Test
+    void shouldCreateUserSuccessfully(){
+        UserDto userDto=new UserDto();
+        userDto.setName("Vinayak");
+        userDto.setEmail("vinayak@gmail.com");
+        userDto.setPassword("12345");
+        userDto.setPhoneNumber(9876897645l);
+        userDto.setAddress("Pune");
+
+        User savedUser=new User();
+        savedUser.setName("Vinayak");
+        savedUser.setEmail("vinayak@gmail.com");
+        savedUser.setPassword("encodedPassword");
+        savedUser.setPhoneNumber(9876897645l);
+        savedUser.setAddress("Pune");
+        savedUser.setRole(Role.GUEST);
+
+        Mockito.when(userRepository.existsByEmail(userDto.getEmail()))
+                .thenReturn(false);
+        Mockito.when(passwordEncoder.encode("12345"))
+                .thenReturn("encodedPassword");
+
+        Mockito.when(userRepository.save(Mockito.any(User.class)))
+                .thenReturn(savedUser);
+
+        UserDto result = userService.createUser(userDto);
+        assertNotNull(result);
+        assertEquals("Vinayak",result.getName());
+        assertEquals("vinayak@gmail.com",result.getEmail());
+        assertEquals(Role.GUEST,result.getRole());
+
+        Mockito.verify(userRepository).save(Mockito.any(User.class));
+        Mockito.verify(passwordEncoder).encode("12345");
 
     }
 }
